7845c71b34b5da5e6cd6092f6d6e6a06d65e69c1
Yash <pandavyash076@gmail.com>
2024-04-30 16:56:12 +0530

    LocalStorage Signin

--- src/componenets/signin.jsx
+++ src/componenets/signin.jsx
@@ -5,6 +5,19 @@ import { useState } from 'react';
 export default function Signin() {
     const [showcurrPasswd, setcurrShowPasswd] = useState(false);
     const [showfinalPasswd, setfinalShowPasswd] = useState(false);
+    const [currPaswd , setCurr] = useState('');
+    const [confmPasswd , setConfm] = useState('');
+
+    const [userData , setUserData] = useState(
+        {
+            fn : "" ,
+            ln : "" ,
+            number : "",
+            email : "" ,
+            confmpasswd : "",
+            userName : ""
+        }
+    );
 
     function currShowPasswdHandler() {
         setcurrShowPasswd((prev) => !prev);
@@ -14,21 +27,43 @@ export default function Signin() {
         setfinalShowPasswd((prev) => !prev);
     }
 
-    const [currPaswd , setCurr] = useState('');
-    const [confmPasswd , setConfm] = useState('');
-
-    function submitHandler(event){
+    function submitHandler(event){    
         if(currPaswd !== confmPasswd) {
-            console.log("NO");
-            return false;
+            event.preventDefault();
+        }   
+    else{
+        if (!localStorage.getItem("user")) {
+            localStorage.setItem("user", JSON.stringify({}));
         }
-        else{
-            console.log("yes");
-            return true;
+        let mainData = JSON.parse(localStorage.getItem("user")) || {};
+        mainData = { ...mainData, [userData.userName]: userData };
+        localStorage.setItem("user", JSON.stringify(mainData));
+        console.log(mainData);
         }
     }
 
+    function dataChangeHandler(event){
+        const {name , value} = event.target;
+        setUserData((prev) => {
+            if(name === "fn" || name === "ln"){
+                const userName = userData.fn + userData.ln + "" + Math.floor(Math.random() * 15);
+                return{
+                    ...prev ,
+                    [name] : value,
+                    userName : userName
+                }
+            }
+            else{
+                return {
+                    ...prev ,
+                    [name] : value,
+                };
+            }
+        });
+    }
+
     return (
+        <>
         <div id='signin'>
             <div id='form'>
                 <h3>
@@ -40,21 +75,21 @@ export default function Signin() {
                     <label htmlFor='fn'>First Name <sup>*</sup>
                     </label>
                     <br></br>
-                    <input type='text' name='fn' required></input>
+                    <input type='text' name='fn' value={userData.fn} required onChange={dataChangeHandler} ></input>
                     <br></br>
                     <label htmlFor='ln'>Last Name <sup>*</sup>
                     </label>
                     <br></br>
-                    <input type='text' name='ln' required></input>
+                    <input type='text' name='ln' value={userData.ln} onChange={dataChangeHandler} required></input>
                     <br></br>
                     <label htmlFor='numb' name='numb'>Contact Number <sup>*</sup>
                     </label>
                     <br></br>
-                    <input type='number' min={0} minLength={10} maxLength={10} name='numb' required></input>
+                    <input type='number' min={0} minLength={10} maxLength={10} name='number' value={userData.number} onChange={dataChangeHandler} required></input>
                     <br></br>
                     <label htmlFor='email'>Email <sup>*</sup></label>
                     <br></br>
-                    <input type='email' name='email' required></input>
+                    <input type='email' name='email' value={userData.email} onChange={dataChangeHandler} required></input>
                     <div style={{ display: "flex", justifyContent: 'space-evenly', alignItems: "center", marginTop: "8px" }}>
                         <div style={{ zIndex: '-100' }}>
                             <label htmlFor='createpasswd'>Create New Password <sup>*</sup></label>
@@ -73,7 +108,17 @@ export default function Signin() {
                         <div style={{ marginLeft: "20px" }}>
                             <label htmlFor='confmpasswd'>Confirm The Password <sup>*</sup></label>
                             <br></br>
-                            <input type={showfinalPasswd ? 'text' : 'password'} value={confmPasswd} name='confmpasswd' onChange={(e) => setConfm(e.target.value)} required />
+                            <input type={showfinalPasswd ? 'text' : 'password'} value={confmPasswd} name='confmpasswd' onChange={ 
+                                    (e) => {setConfm(e.target.value)
+                                            setUserData((prev) => {
+                                                return {
+                                                    ...prev ,
+                                                    [e.target.name] : e.target.value,
+                                                };
+                                            });
+                                    }
+
+                                    } required />
                             <button type='button' className='passbtn' onClick={finalShowpasswdHandler} >
                                 {
                                     !showfinalPasswd ? (
@@ -92,5 +137,6 @@ export default function Signin() {
             </div>
             <img id='signupimg' src={require('../data/two.png')} alt="signup image" />
         </div>
+        </>
     );
 }
