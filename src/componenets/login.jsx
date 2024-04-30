b4a5249114a4ff6cca76b97eb77a62a7721fc310
Yash <pandavyash076@gmail.com>
2024-04-30 19:54:00 +0530

    Completed 100% login and signup

--- src/componenets/login.jsx
+++ src/componenets/login.jsx
@@ -1,14 +1,84 @@
 import './login.css';
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 import { useState} from 'react';
+import { toast } from "react-hot-toast";
 
 export default function Login(){
     const [showpaswd, setshow] = useState(false);
-
     const [loginUsing , setLogin] = useState('username');
+    const [currUser , setCurr] = useState(
+        {
+            loginId : "",
+            passwd : ""
+        }
+    )
+    const mainData = JSON.parse(localStorage.getItem("user"));
 
-    function showhandler() {
+    function showhandler() { //show password
         setshow((prev) => !prev);
+    }
+
+    function changeHandler(event){ //adding details into the object
+        const {name , value} = event.target;
+
+        if(name === 'username' || name === 'email'){
+            setCurr((prev) => {
+                return {
+                    ...prev ,
+                    loginId : value
+                }
+            })
+        }
+
+        else{setCurr((prev) => {
+            return {
+                ...prev ,
+                passwd : value
+            }
+        })
+        }
+    }
+
+    function submitHandler(event){  // onsubmit
+        let dataFound = false;
+        let getKey = '';
+        if(mainData){       
+            if(loginUsing === 'email'){
+                Object.keys(mainData).forEach(key => {
+                    if(mainData[key].email === currUser.loginId){
+                        dataFound = !dataFound;
+                        getKey = key;
+                    }
+                })
+            }
+            else if(loginUsing === 'username'){
+                    Object.keys(mainData).forEach(key => {
+                    if(key === currUser.loginId){
+                        dataFound = !dataFound;
+                        getKey = key;
+                    }
+                });
+            }
+            if(!dataFound){
+                toast.error("No such data found , Create a new account");
+                event.preventDefault();
+                return;
+            }
+            if(dataFound){
+                if(mainData[getKey].confmpasswd !== currUser.passwd){
+                    toast.error("Password hasn't matched");
+                    event.preventDefault();
+                    return;
+                }
+                else{
+                    toast.success("Login Success");
+                }
+            } 
+        }
+        else{
+            toast.error("No such data found , Create a new account");
+            event.preventDefault();
+        }
     }
     
     return(
@@ -28,27 +98,27 @@ export default function Login(){
                         <button type='button' onClick={() => {setLogin('username')}} className='emailorusername'> username
                         </button></sub>
                 </p>
-                <form>
+                <form onSubmit={submitHandler}>
                     {
                         loginUsing === 'username' ? (
                             <>
                             <label htmlFor='username'>Username <sup>*</sup>
                             </label>
                             <br></br>
-                            <input type='text' name='username' required></input>
+                            <input type='text' name='username' required value={currUser.loginId} onChange={changeHandler}></input>
                             <br></br>
                             </>
                         ) : (
                             <>
                             <label htmlFor='email'>Email <sup>*</sup></label>
                             <br></br>
-                            <input type='email' name='email' required></input>
+                            <input type='email' name='email' required value={currUser.loginId} onChange={changeHandler}></input>
                             <br></br>
                             </>
                         )
                     }
                     <label htmlFor='passwd'name='passwd' require>Password <sup>*</sup></label>
-                    <input type={showpaswd ? 'text' : 'password'} required />
+                    <input type={showpaswd ? 'text' : 'password'} name='passwd' value={currUser.passwd} onChange={changeHandler} required />
                     <button type='button' className='passbtn' onClick={showhandler} >
                                 {
                                     !showpaswd ? (
@@ -68,9 +138,17 @@ export default function Login(){
                     </div>
                     <hr style={{ backgroundColor: "grey" }}></hr>
                     <button type='submit' id='submit'>Log In</button>
-                    <button type='reset'>Reset</button>
+                    <button type='reset' onClick={() =>{
+                        setCurr((prev) => {
+                            return {
+                                ...prev ,
+                                loginId : "",
+                                passwd : ""
+                            }
+                        })
+                    }} >Reset</button>
                 </form>
             </div>
         </div>
     );
-}
+} 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { Toaster } from "react-hot-toast";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />    
-        <ToastContainer></ToastContainer>
+        <Toaster />
     </BrowserRouter>
 );
