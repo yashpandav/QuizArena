8ef19477119a678b12dd4efe25c13b6a49a50aad
Yash <pandavyash076@gmail.com>
2024-05-04 22:51:04 +0530

    Exam Filterd

--- src/componenets/mainExam.jsx
+++ src/componenets/mainExam.jsx
@@ -1,11 +1,61 @@
-
 export default function MainExam({examCategory , filteredData}){
-    console.log(filteredData);
+
+    // const examData = {
+    //     "category" : filteredData.question,
+    //     "answer" : filteredData.
+    // }
+
+    const examArr = [];
+    let start = Math.floor(Math.random() * 50);
+    let end = start + 10;
+    
+    for(let i = start ; i < end ;){
+        filteredData.forEach(element => {
+            let idx = Math.floor(Math.random() * 50)
+            const ele = element.id === idx ? element : "";
+            if(ele !== ""){
+                examArr.push(ele);
+                i++;
+            }
+            else{
+            }
+        });
+    }
+
+    if(examArr.length > 10){
+        examArr.splice(10 , examArr.length);
+    }
+    console.log(examArr)
 
     return(
         <div className="mainExam">
             <h5>Exam Type : {examCategory}</h5>
             <hr style={{width : "100vw" , height : "0.5px" , backgroundColor : "white"}}></hr>
-        </div>
+            <div className="examContainer">
+                <div>
+                    {}
+                    <h6>{filteredData.question}</h6>
+                </div>
+            </div>
+        </div> 
     )
 }
+
+
+// 37
+// : 
+// answer
+// : 
+// "C"
+// category
+// : 
+// "Programming"
+// id
+// : 
+// 59
+// options
+// : 
+// {A: 'Java', B: 'Python', C: 'C#', D: 'Ruby'}
+// question
+// : 
+// "Which of the following is a commonly used programming language for game development?"
